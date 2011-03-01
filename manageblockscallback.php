17fed5893e5076d3d84eae4ace07efa7a9b77c91
Ian Renton <ian@onlydreaming.net>
2011-03-01 15:18:07 -0800

    Now with one less bracket.

--- manageblockscallback.php
+++ manageblockscallback.php
@@ -6,17 +6,16 @@ require_once('twitteroauth/twitteroauth.php');
 require_once('config.php');
 
 if ((isset($_SESSION['sw_uid'])) && (isset($_POST['blocklist']))) {
-        mysql_connect(DB_SERVER,DB_USER,DB_PASS);
-        @mysql_select_db(DB_NAME) or die( "Unable to select database");
+    mysql_connect(DB_SERVER,DB_USER,DB_PASS);
+    @mysql_select_db(DB_NAME) or die( "Unable to select database");
 
-			// Update table
-			$query = "UPDATE sw_users SET blocklist='" . mysql_real_escape_string($_POST['blocklist']) . "' WHERE sw_uid='" . mysql_real_escape_string($_SESSION['sw_uid']) . "'";
-			mysql_query($query);
-			
-			mysql_close();
-			header('Location: ./index.php');
-    		die();
-		}
+		// Update table
+		$query = "UPDATE sw_users SET blocklist='" . mysql_real_escape_string($_POST['blocklist']) . "' WHERE sw_uid='" . mysql_real_escape_string($_SESSION['sw_uid']) . "'";
+		mysql_query($query);
+		
+		mysql_close();
+		header('Location: ./index.php');
+		die();
 } else {
     // Called without username/blocklist POST.
     header('Location: ./manageblocks.php?fail=true');
