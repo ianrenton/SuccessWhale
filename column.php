c7736e5f0598e3621cf31bfc944781e1d152b513
Ian Renton <ian@onlydreaming.net>
2011-03-01 15:09:31 -0800

    Fixed a couple more database issues where blocklists weren't working and changed columns weren't updating the DB.

--- column.php
+++ column.php
@@ -5,11 +5,8 @@ date_default_timezone_set('UTC');
 require_once('common.php');
 session_start();
 
-if (DB_SERVER != '') {
-    // Connect to DB, we will be using this a lot!
-    mysql_connect(DB_SERVER,DB_USER,DB_PASS);
-    @mysql_select_db(DB_NAME) or die( "Unable to select database");
-}
+mysql_connect(DB_SERVER,DB_USER,DB_PASS);
+@mysql_select_db(DB_NAME) or die( "Unable to select database");
     
 $content = '';
 
@@ -29,34 +26,30 @@ if (isset($_GET['count'])) {
 $paramArray["per_page"] = $paramArray["count"];
 
 
-if (DB_SERVER != '') {
-	// If updatedb=1, update the userprefs in the database with the new column info
-    if (isset($_GET['column']) && isset($_GET['div']) && isset($_GET['updatedb']) && ($_GET['updatedb'] == '1')) {
-        //TODO replace column1 with column when renaming db field
-        $query = "SELECT column1 FROM userprefs WHERE username = '" . mysql_real_escape_string($thisUser) . "'";
-        $result = mysql_query($query);
-        if (mysql_num_rows($result) > 0) {
-            $userprefrow = mysql_fetch_assoc($result);
-            //TODO replace column1 with column when renaming db field
-            $userCols = explode(";", $userprefrow['column1']);
-            $userCols[($_GET['div'])] = $_GET['column'];
-            $newColsString = implode (";", $userCols);
-            //TODO replace column1 with column when renaming db field
-            $query = "UPDATE userprefs SET column1 = '" . mysql_real_escape_string($newColsString) . "' WHERE username = '" . mysql_real_escape_string($thisUser) . "'";
-            mysql_query($query);
-        }
-    }
-
-	// Get blocklist
-	$query = "SELECT * FROM userprefs WHERE username='" . mysql_real_escape_string($_SESSION['thisUser']) . "'";
+// If updatedb=1, update the users table in the database with the new column info
+if (isset($_GET['column']) && isset($_GET['div']) && isset($_GET['updatedb']) && ($_GET['updatedb'] == '1')) {
+    $query = "SELECT columns FROM sw_users WHERE sw_uid = '" . mysql_real_escape_string($_SESSION['sw_uid']) . "'";
     $result = mysql_query($query);
-    $userprefs = mysql_fetch_assoc($result);
-    if ($userprefs != FALSE) {
-        $blocklist = $userprefs["blocklist"];
-    } else {
-        $blocklist = "";
+    if (mysql_num_rows($result) > 0) {
+        $row = mysql_fetch_assoc($result);
+        $userCols = explode(";", $row['columns']);
+        $userCols[($_GET['div'])] = $_GET['column'];
+        $newColsString = implode (";", $userCols);
+        $query = "UPDATE sw_users SET columns = '" . mysql_real_escape_string($newColsString) . "' WHERE sw_uid = '" . mysql_real_escape_string($_SESSION['sw_uid']) . "'";
+        mysql_query($query);
     }
 }
+
+// Get blocklist
+$query = "SELECT blocklist FROM sw_users WHERE sw_uid='" . mysql_real_escape_string($_SESSION['sw_uid']) . "'";
+$result = mysql_query($query);
+$row = mysql_fetch_assoc($result);
+if ($row != FALSE) {
+    $blocklist = $userprefs["blocklist"];
+} else {
+    $blocklist = "";
+}
+
 
 // Get column-dependent data and render
 if (isset($_GET['column'])) {
@@ -72,7 +65,7 @@ if (isset($_GET['column'])) {
 	    $url = $matches[1] . "/lists/" . $listStub . "/statuses";
 	//} else if (preg_match("/^#(\w+)$/", $name, $matches)) {
 	    // Matches #tag
-	    // Don't know how to handle this, saved searches?
+	    // TODO implement searches
 	    //$url = $name;
 	} else {
 	    // Doesn't match, assume it's a real URL like "statuses/mentions".
@@ -110,9 +103,6 @@ if (isset($_GET['column'])) {
     echo $content;
 }
 
-// End of script, close DB.
-if (DB_SERVER != '') {
-    mysql_close();
-}
+mysql_close();
 
 ?>
