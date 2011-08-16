f5642a555d7ebafb751d56a92f216ca20bd256fe
tsuki <ian@onlydreaming.net>
2011-08-16 14:45:55 -0700

    Reply box as a textarea rather than an input. Requested by @rgenzon long ago, I think.

--- replybox.php
+++ replybox.php
@@ -4,12 +4,12 @@ require_once('common.php');
 session_start();
 
 $content .= '<div id="replybox">';
-$content .= '<input type="text" autocomplete="off" class="reply" value="' . $_GET['initialtext'] . ' ">';
+$content .= '<textarea autocomplete="off" class="reply">' . $_GET['initialtext'] . '</textarea>';
 
 $content .= '<input type="hidden" class="replyid" value="' . $_GET['replyid'] . '" />';
 $content .= '<input type="hidden" class="account" value="' . $_GET['account'] . '" />';
+$content .= '<span class="counter" style="left:0px;padding:0 5px;">' . strlen($_GET['initialtext']) . '</span>';
 $content .= '<input type="submit" class="replybutton" value="Post" />';
-$content .= '<span class="counter">' . strlen($_GET['initialtext']) . '</span>';
 $content .= '</div>';
 
 echo $content;
