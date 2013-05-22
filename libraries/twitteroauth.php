332bc05a987e7d0a7915d629bc0b6923e62ef173
Ian Renton <ian@ianrenton.com>
2013-05-22 21:35:27 +0100

    Quick hack to move to API v1.1, checking if this works.

--- libraries/twitteroauth.php
+++ libraries/twitteroauth.php
@@ -22,7 +22,7 @@ class TwitterOAuth {
   /* Contains the last API call */
   public $last_api_call;
   /* Set up the API root URL */
-  public $host = "https://api.twitter.com/1/";
+  public $host = "https://api.twitter.com/1.1/";
   /* Set timeout default */
   public $timeout = 30;
   /* Set connect timeout */
