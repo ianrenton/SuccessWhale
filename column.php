dd0a2fe01140dce9f422ae60ccc1d01bfadf36c7
Ian Renton <ian@ianrenton.com>
2013-06-27 21:04:01 +0100

    Switch Twitter's mentions timeline URL from statuses/mentions to statuses/mentions_timeline

--- column.php
+++ column.php
@@ -121,7 +121,7 @@ if (isset($_GET['column'])) {
 	            $listStub = strtolower(str_replace(" ", "-", $matches[2]));
 	            $url = $matches[1] . "/lists/" . $listStub . "/statuses";
 	        } else {
-	            // Doesn't match, assume it's a real URL like "statuses/mentions".
+	            // Doesn't match, assume it's a real URL like "statuses/mentions_timeline".
 	            $url = $name;
 	        }
 	
@@ -133,6 +133,10 @@ if (isset($_GET['column'])) {
                         $url = "lists/statuses";
                         $paramArray['owner_screen_name'] = $matches[1];
                         $paramArray['slug'] = $matches[2];
+                    }
+                    // Fix mentions timeline for API v1.1
+                    if ($url == "statuses/mentions") {
+                        $url = "statuses/mentions_timeline";
                     }
                     
 	                $data = $twitter->get($url, $paramArray);
