d6b48ae1d1107b125594ac9b9137cb1621c494d1
Ian Renton <ian@ianrenton.com>
2014-05-02 21:58:02 +0100

    Fiddling with Facebook auth support

---
+++ public/js/successwhale-facebookcallback.js
@@ -0,0 +1,44 @@
+// Globals
+var API_SERVER = 'https://successwhale-api.herokuapp.com/v3';
+var COOKIE_VALIDITY_DAYS = 365;
+
+// Get GET params
+function getSearchParameters() {
+      var prmstr = window.location.search.substr(1);
+      return prmstr != null && prmstr != "" ? transformToAssocArray(prmstr) : {};
+}
+
+function transformToAssocArray( prmstr ) {
+    var params = {};
+    var prmarr = prmstr.split("&");
+    for ( var i = 0; i < prmarr.length; i++) {
+        var tmparr = prmarr[i].split("=");
+        params[tmparr[0]] = tmparr[1];
+    }
+    return params;
+}
+var params = getSearchParameters();
+
+
+// Automatically submit callback to SW and reload
+$(document).ready(function() {
+
+  var paramsToPassOn = {callback_url: escape(location.origin+'/facebookcallback'), code: params.code};
+  // Get token from cookie if we're already logged in, so the API binds the new Facebook
+  // account to the right user
+  if (readCookie('token')) {
+    paramsToPassOn[token] = readCookie('token');
+  }
+  
+  alert(params.code);
+  
+  var jqxhr = $.get(API_SERVER+'/authwithfacebook', paramsToPassOn)
+    .done(function(returnedData) {
+      window.location = '/';
+    })
+    .fail(function(returnedData) {
+      alert(returnedData.error);
+      window.location = '/';
+    });
+  
+});
