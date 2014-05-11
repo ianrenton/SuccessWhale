b19f0dcbae6c3512739e3bae68ce406d5b619b1a
Ian Renton <ian@ianrenton.com>
2014-05-11 11:34:17 +0100

    Tidying

--- public/js/successwhale.js
+++ public/js/successwhale.js
@@ -46,7 +46,7 @@ ko.bindingHandlers.slideVisible = {
         $(element).toggle(ko.unwrap(value)); // Use "unwrapObservable" so we can handle values that may or may not be observable
     },
     update: function(element, valueAccessor) {
-        // Whenever the value subsequently changes, slowly fade the element in or out
+        // Whenever the value subsequently changes, slide the element in or out
         var value = valueAccessor();
         ko.unwrap(value) ? $(element).slideDown() : $(element).slideUp();
     }
