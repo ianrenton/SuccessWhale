90aabe927826a7c3e3e498cd54092eb63340bb0a
Ian Renton <ian@ianrenton.com>
2017-10-26 17:33:18 +0100

    Ready API URL for loss of SuccessWhale.com

--- js/common.js
+++ js/common.js
@@ -1,5 +1,5 @@
 // Globals
-var API_SERVER = 'https://api.successwhale.com/v3';
+var API_SERVER = 'https://successwhale-api.herokuapp.com/v3';
 var NARROW_SCREEN_WIDTH = 800;
 var COOKIE_VALIDITY_DAYS = 365;
 
